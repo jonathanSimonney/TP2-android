bcb12a37618434619fa932d48388a130daec8d01
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-10-18 00:25:35 +0200

    toolbar and bottomNavigationView setup.

--- app/src/main/java/com/example/jonathansimonney/igeneration/MainActivity.kt
+++ app/src/main/java/com/example/jonathansimonney/igeneration/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.jonathansimonney.igeneration
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setToolbarTitle()
+    }
+
+    private fun setToolbarTitle() {
+        val myToolbar = findViewById<View>(R.id.z_toolbar) as Toolbar
+        myToolbar.title = "Actualités"
+        setSupportActionBar(myToolbar)
     }
 }
