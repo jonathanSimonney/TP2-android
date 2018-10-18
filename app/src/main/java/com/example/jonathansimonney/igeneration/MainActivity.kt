29bdfc23334b9455965c02f9eecf114d2eb11e4f
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-10-18 10:43:04 +0200

    added fragment linked with menu.

--- app/src/main/java/com/example/jonathansimonney/igeneration/MainActivity.kt
+++ app/src/main/java/com/example/jonathansimonney/igeneration/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.jonathansimonney.igeneration
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.view.menu.MenuView
 import android.support.v7.widget.Toolbar
 import android.view.View
+import com.example.jonathansimonney.igeneration.R.id.index
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,11 +19,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setToolbarTitle()
+        setFragment(NewsFragment.newInstance())
+        setMenuListener()
+    }
+
+    private fun setFragment(fragment :Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.myFragmentContainer, fragment)
+        fragmentTransaction.commit()
     }
 
     private fun setToolbarTitle() {
         val myToolbar = findViewById<View>(R.id.z_toolbar) as Toolbar
         myToolbar.title = resources.getString(R.string.news)
         setSupportActionBar(myToolbar)
+    }
+
+    private fun setMenuListener(){
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.action_news -> setFragment(NewsFragment.newInstance())
+                R.id.action_books -> setFragment(BooksFragment.newInstance())
+                R.id.action_forum -> setFragment(ForumFragment.newInstance())
+                R.id.action_club_igen -> setFragment(ClubIgenFragment.newInstance())
+                R.id.action_settings -> setFragment(SettingsFragment.newInstance())
+            }
+            true
+        }
     }
 }
