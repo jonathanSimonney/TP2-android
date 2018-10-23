a0e9611d9e2f32a08ffeab970de827c5d83ce53c
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-10-23 12:05:33 +0200

    swipe to refresh functioning.

--- app/src/main/java/com/example/jonathansimonney/igeneration/NewsFragment.kt
+++ app/src/main/java/com/example/jonathansimonney/igeneration/NewsFragment.kt
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_news.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -39,6 +40,13 @@ class NewsFragment : Fragment() {
         // Inflate the layout for this fragment
 
         return inflater.inflate(R.layout.fragment_news, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        stopRefreshButton.setOnClickListener {
+            refreshingSwipe.isRefreshing = false
+        }
     }
 
     companion object {
