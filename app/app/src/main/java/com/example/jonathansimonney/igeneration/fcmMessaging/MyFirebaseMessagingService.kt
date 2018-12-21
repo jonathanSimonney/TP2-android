a87824c90d6fffe24aeadcae4431846b586e3989
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-12-21 12:41:47 +0100

    notification now displayed on the phone.

--- app/app/src/main/java/com/example/jonathansimonney/igeneration/fcmMessaging/MyFirebaseMessagingService.kt
+++ app/app/src/main/java/com/example/jonathansimonney/igeneration/fcmMessaging/MyFirebaseMessagingService.kt
@@ -1,16 +1,20 @@
 package com.example.jonathansimonney.igeneration.fcmMessaging
 
+import android.support.v4.app.NotificationCompat
 import android.util.Log
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-
+import android.app.NotificationManager
+import android.content.Context
+import com.example.jonathansimonney.igeneration.R
 
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     private lateinit var database: DatabaseReference
 
+    private val NEW_ARTICLE_NOTIFICATION_ID = 1
     // Méthode appelée chaque fois que le token change
     /**
      * Called if InstanceID token is updated. This may occur if the security of
@@ -37,5 +41,16 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // On récupère les données envoyées
         message?.data
         Log.d("jonathanMessaging", message?.data.toString())
+        val notificationTitle = message?.data?.get("title")
+        val notificationContent = "Découvrez le dernier article écrit par " + message?.data?.get("author")
+
+        val newArticleNotification = NotificationCompat.Builder(applicationContext, "newArticleChannel")
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationContent)
+                .setSmallIcon(R.drawable.new_icon)
+                .build()
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(NEW_ARTICLE_NOTIFICATION_ID, newArticleNotification)
     }
 }
